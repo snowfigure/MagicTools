486d715828a33056c2f4960df282334434b75341
snowfigure <fdqlbf@126.com>
2017-11-12 22:02:22 +0800

    日志

---
+++ MagicTool.Util/Log.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MagicTool.Util
+{
+    public static class Log
+    {
+        public static void Debug(string log_info)
+        {
+            ;
+        }
+
+        public static void Run(string log_info)
+        {
+            ;
+        }
+
+        public static void Error(string log_info)
+        {
+            ;
+        }
+
+
+    }
+}
